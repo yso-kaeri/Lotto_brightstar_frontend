4ccc378eed60a7d3a7fb213892bdb6f39683e5e1
yso-kaeri <yso.kaeri@gmail.com>
2025-07-07 16:26:16 +0900

    增加代码使得可像浏览器一样开发的时候刷新

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// 用于像浏览器一样开发的时候刷新 20250707kos
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
