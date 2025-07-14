ff18ff08677f85d6a412280341d9583a216ac37d
Sawa8811 <sawa88101@gmail.com>
2025-07-14 14:05:45 +0900

    打包用

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-// 用于像浏览器一样开发的时候刷新 20250707kos
-require('electron-reload')(__dirname, {
-    electron: require(`${__dirname}/node_modules/electron`)
-});
+// 開啟時啟動後端jar
+const { exec } = require('child_process');
+exec('java -jar ./Lotto_brightstar-0.0.1-SNAPSHOT.jar', { cwd: __dirname });
+
+// // 用于像浏览器一样开发的时候刷新 20250707kos
+// require('electron-reload')(__dirname, {
+//     electron: require(`${__dirname}/node_modules/electron`)
+// });
+
+// const { app, BrowserWindow } = require('electron');
+// const path = require('path');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
